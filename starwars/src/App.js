906a281ed9bcbb091341d95c79c1a496bc97c8ad
wtrawlings <rawlings.warren@gmail.com>
2019-10-27 20:55:57 -0400

    completed styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import './App.css';
 import TarotCard from "./components/TarotCard"
 
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -11,8 +12,8 @@ const App = () => {
     axios
       .get('https://swapi.co/api/people/')
       .then(response => {
-        console.log(response.data);
-        setCharacters(response.data);
+        console.log(response.data.results);
+        setCharacters(response.data.results);
       })
       .catch(error => {
         console.log(error);
@@ -24,18 +25,27 @@ const App = () => {
   // sync up with, if any.
 
   return (
+    <>
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header">React Wars: Personnel Report</h1>
     </div>
     <div>
       {characters.map((character, id) => {
         return (
           <div>
-
+            <TarotCard 
+            name={character.name}
+            species={character.species}
+            skin_color={character.skin_color}
+            hair_color={character.hair_color}
+            height={character.height}
+            mass={character.mass}
+            />
           </div>
         )
       })}
     </div>
+    </>
   );
 }
 
--- starwars/src/components/TarotCard.js
+++ starwars/src/components/TarotCard.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    color: white;
+    background-color: grey;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 10px;
+`
 
 const TarotCard = props => {
     return (
-        <div className="description">
+        <StyledDiv className="description">
             <h2>NAME: {props.name}</h2>
             <p>SPECIES: {props.species}</p>
             <p>SKIN COLOR: {props.skin_color}</p>
             <p>HAIR COLOR: {props.hair_color}</p>
-            <p>HEIGHT: {props.height}cm</p>
+            <p>HEIGHT: {props.height}</p>
             <p>MASS: {props.mass}kg</p>
-        </div>
+        </StyledDiv>
     );
 }
 
